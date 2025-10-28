92b86e536f32c9ff1b17b7b64c25f81386f9e08b
EmilioQuinonez <emiliosantiago050810@gmail.com>
2025-10-28 12:11:22 -0600

    ADD: Se añade las carpetas para la arquitectura

---
+++ NefroVida/NefroVida/NefroVidaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NefroVidaApp.swift
+//  NefroVida
+//
+//  Created by Emilio Santiago López Quiñonez on 28/10/25.
+//
+
+import SwiftUI
+
+@main
+struct NefroVidaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NefroVida/NefroVidaTests/NefroVidaTests.swift
@@ -0,0 +1,17 @@
+//
+//  NefroVidaTests.swift
+//  NefroVidaTests
+//
+//  Created by Emilio Santiago López Quiñonez on 28/10/25.
+//
+
+import Testing
+@testable import NefroVida
+
+struct NefroVidaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
