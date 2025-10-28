1565850e57995f9d0ec94496a4affb1f4e16e893
Manuel <mbajosr@gmail.com>
2025-10-28 10:55:36 -0600

    create folders

---
+++ VitalSoft/VitalSoft/VitalSoftApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitalSoftApp.swift
+//  VitalSoft
+//
+//  Created by Manuel Bajos Rivera on 28/10/25.
+//
+
+import SwiftUI
+
+@main
+struct VitalSoftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VitalSoft/VitalSoftTests/VitalSoftTests.swift
@@ -0,0 +1,16 @@
+//
+//  VitalSoftTests.swift
+//  VitalSoftTests
+//
+//  Created by Manuel Bajos Rivera on 28/10/25.
+//
+
+import Testing
+
+struct VitalSoftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
