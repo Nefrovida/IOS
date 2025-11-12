67f202f27b4a6ecf7fac9435e54ffe74b70361b9
EmilioQuinonez <emiliosantiago050810@gmail.com>
2025-11-12 02:23:20 -0600

    Add: The loginModel is created

---
+++ NefrovidaApp/NefrovidaApp/Data/Model/LoginModel.swift
@@ -0,0 +1,23 @@
+//
+//  LoginModel.swift
+//  NefrovidaApp
+//
+//  Created by Emilio Santiago López Quiñonez on 12/11/25.
+//
+
+import Foundation
+
+// Used to decode the JSON response from the server
+nonisolated struct LoginResponse: Decodable {
+    // Contains user data
+    let user: LoginModel
+}
+
+//  Model representing the user based on the received JSON
+nonisolated struct LoginModel: Decodable, Sendable {
+    let user_id: String
+    let name: String
+    let username: String
+    let role_id: Int
+    let privileges: [String]?
+}
