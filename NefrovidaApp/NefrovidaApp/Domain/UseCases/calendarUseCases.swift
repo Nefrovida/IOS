807dd5d656749ccf4a862ff5e511040bd6cd1c18
Manu090704 <A01711581@tec.mx>
2025-11-12 13:14:20 -0600

    add: comments

--- NefrovidaApp/NefrovidaApp/Domain/UseCases/calendarUseCases.swift
+++ NefrovidaApp/NefrovidaApp/Domain/UseCases/calendarUseCases.swift
@@ -10,31 +10,26 @@ final class GetAppointmentsForDayUseCase {
     private let workHoursRange = 8...18
 
     // Initial of the use case.
-    // - Parameter repository: Implementación concreta del repositorio (por ejemplo, RemoteAppointmentRepository).
+    // - Parameter repository: Implemention of the repository
     init(repository: AppointmentRepository) {
         self.repository = repository
     }
 
-    // Ejecuta la lógica principal del caso de uso.
-    // - Obtiene todas las citas del repositorio (incluye citas médicas y análisis).
-    // - Filtra únicamente las citas correspondientes al día indicado.
-    // - En esta versión, no se aplica aún el filtro por horario laboral.
-    //
-    // - Parameters:
-    //   - date: Fecha que se desea consultar en formato "yyyy-MM-dd".
-    //   - idUser: Identificador único del usuario autenticado.
-    // - Returns: Lista filtrada de citas del día.
-    // - Throws: Propaga errores de red o de decodificación ocurridos en el repositorio.
+    // Get the appointments from the repository and apply a filter
+    // Parameters: date: date of the appointment.
+    // idUser: Id of the user in the sesion.
+    // Returns: List of the appointment with the filter.
+    // Throws: Error if the conection fails or the decode.
     func execute(date: String, idUser: String) async throws -> [Appointment] {
-        // Solicita todas las citas del repositorio (sin filtrar).
+        // Get the appointment without filter.
         let allAppointments = try await repository.fetchAppointments(idUser: idUser)
         
-        // Filtra únicamente las citas cuya fecha (dateHour) comience con la fecha seleccionada.
+        // Apply the filter with the date selected.
         let sameDay = allAppointments.filter {
             $0.dateHour.starts(with: date)
         }
         
-        // Retorna solo las citas del día solicitado.
+        // Return the appointment of that selected date.
         return sameDay
     }
 }
