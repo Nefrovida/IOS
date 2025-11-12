3c1a7302204f8cf2d8f9bef465b12d8a62ddd3f9
Manu090704 <A01711581@tec.mx>
2025-11-12 12:01:29 -0600

    delete: remove calendar

--- NefrovidaApp/NefrovidaApp/ContentView.swift
+++ NefrovidaApp/NefrovidaApp/ContentView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        Text("Hello, World!")
         
-        AgendaScreen(idUser: "35eb038c-3c7a-4143-9f6a-9c8c7d70de97")
     }
 }
 
