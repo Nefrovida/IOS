8f19d2339b4020d78a9578b63ea9e339526a8b40
EmilioQuinonez <emiliosantiago050810@gmail.com>
2025-11-14 22:16:49 -0600

    Fix: BottomBar output messages are corrected

--- NefrovidaApp/NefrovidaApp/Components/Molecules/BottomBar.swift
+++ NefrovidaApp/NefrovidaApp/Components/Molecules/BottomBar.swift
@@ -3,13 +3,13 @@ import SwiftUI
 struct BottomBar: View {
     var body: some View {
         HStack{
-            buttonBar(imageName:"house", text: "Inicio"){ print("hola")}
+            buttonBar(imageName:"house", text: "Inicio"){ print("Home")}
             Spacer()
-            buttonBar(imageName:"testtube.2", text: "Analisis"){ print("hola")}
+            buttonBar(imageName:"testtube.2", text: "Analisis"){ print("Analisis")}
             Spacer()
-            buttonBar(imageName:"calendar", text: "Agenda"){ print("hola")}
+            buttonBar(imageName:"calendar", text: "Agenda"){ print("Agenda")}
             Spacer()
-            buttonBar(imageName:"text.bubble", text: "Foros"){ print("hola")}
+            buttonBar(imageName:"text.bubble", text: "Foros"){ print("Foros")}
         }.padding(.horizontal, 22)
             .padding(.vertical, 10)
             .background(.ultraThinMaterial)
