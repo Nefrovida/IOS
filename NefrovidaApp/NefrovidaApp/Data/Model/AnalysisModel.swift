a978b603458cbd7fc8380bfaf1ae51168d5fe590
Manu090704 <A01711581@tec.mx>
2025-11-16 15:54:35 -0600

    model of consultation

--- NefrovidaApp/NefrovidaApp/Data/Model/AnalysisModel.swift
+++ NefrovidaApp/NefrovidaApp/Data/Model/AnalysisModel.swift
@@ -6,6 +6,24 @@ struct AnalysisResponse: Codable, Sendable {
     let data: [Analysis]
 }
 
+struct Consultation: Codable, Sendable {
+    let appointmentId: Int
+    let doctorId: String
+    let nameConsultation: String
+    let generalCost: Int
+    let communityCost: Int
+    let img: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case appointmentId = "appointment_id"
+        case doctorId = "doctor_id"
+        case nameConsultation = "name"
+        case generalCost = "general_cost"
+        case communityCost = "community_cost"
+        case img = "image_url"
+    }
+}
+
 struct Analysis: Codable, Identifiable, Sendable {
     let id: Int
     let name: String
