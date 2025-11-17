bcc4fab5ffd1b0cb45162ea5cb692f9aca1a65c1
EmilioQuinonez <emiliosantiago050810@gmail.com>
2025-11-16 23:06:56 -0600

    Add: A temporary connection is added to the appointmentView view

--- NefrovidaApp/NefrovidaApp/Presentation/View/homeView.swift
+++ NefrovidaApp/NefrovidaApp/Presentation/View/homeView.swift
@@ -7,25 +7,40 @@
 
 import SwiftUI
 
-import SwiftUI
-
 struct HomeView: View {
     let user: LoginEntity?
-    
+
     var body: some View {
-        VStack(spacing: 0){
-            UpBar()
-            
-            VStack {
-                Text("Bienvenido, \(user?.name ?? "")")
-                    .font(.title)
-                    .padding()
-            }.frame(maxWidth: .infinity, maxHeight: .infinity)
+        NavigationStack {
+            VStack(spacing: 0) {
+                UpBar()
+                VStack(spacing: 20) {
+                    Text("Bienvenido, \(user?.name ?? "")")
+                        .font(.title)
+                        .padding(.top, 20)
+                    
+                    NavigationLink(
+                        destination: appointmentView(appointmentId: 1,
+                                                     userId: user?.user_id ?? "")
+                    ) {
+                        Text("Agendar cita")
+                            .font(.headline)
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color.blue.opacity(0.8))
+                            .foregroundColor(.white)
+                            .cornerRadius(12)
+                            .padding(.horizontal, 24)
+                    }
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                
+                BottomBar()
+            }
         }
-        
-        BottomBar()
     }
 }
+
 #Preview {
     HomeView(
         user: LoginEntity(
