902c3ee5004c9387f633886d7364a3f1cdde0a83
EmilioQuinonez <emiliosantiago050810@gmail.com>
2025-12-03 17:11:41 -0600

    Update: The UpBar is update to manage two diferent design

--- NefrovidaApp/NefrovidaApp/Components/Molecules/UpBar.swift
+++ NefrovidaApp/NefrovidaApp/Components/Molecules/UpBar.swift
@@ -1,30 +1,46 @@
 import SwiftUI
 
 struct UpBar: View {
+    var showSidebar: Binding<Bool>? = nil
     var body: some View {
         ZStack {
             Rectangle()
                 .fill(.ultraThinMaterial)
                 .frame(height: 75)
                 .shadow(color: Color.black.opacity(0.2), radius: 6, x: 0, y: 6)
-            HStack{
-                NavigationLink(destination: ProfileView()) {
-                    VStack {
+
+            if let showSidebar = showSidebar {
+                HStack {
+                    Button(action: {
+                        withAnimation {
+                            showSidebar.wrappedValue.toggle()
+                        }
+                    }) {
                         Image(systemName: "person.crop.circle")
-                        Text("")
+                            .font(.system(size: 26))
+                            .padding(10)
+                            .foregroundColor(.black)
                     }
-                    .padding(10)
-                    .foregroundColor(.black)
+                    .font(.system(size: 26))
+
+                    NefroVidaLogo()
+
+                    buttonBar(imageName: "bell", text: "") {
+                        print("Notificaciones")
+                    }
+                    .font(.system(size: 26))
                 }
-                .font(.system(size:26))
-                NefroVidaLogo()
-                buttonBar(imageName:"bell", text: ""){ print("Notificaciones")}
-                    .font(.system(size:26))
-            }.padding(.horizontal, 22)
+                .padding(.horizontal, 22)
                 .padding(.vertical, 10)
+            } else {
+                HStack {
+                    Spacer()
+                    NefroVidaLogo()
+                    Spacer()
+                }
+                .padding(.horizontal, 22)
+                .padding(.vertical, 10)
+            }
         }
     }
-}
-#Preview {
-    UpBar()
 }
